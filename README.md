bd6b8683b296ce588f6328dadbdfcc3c696ca801
root <diegoferreira1964@gmail.com>
2021-10-23 13:47:22 -0300

    Novos nome de pastas e correcao de erro de chav no frontend

---
+++ README.md
@@ -0,0 +1,60 @@
+<h1 align="center">👨🏾‍🚀🚀 NextLevelWeek - Heat</h1>
+
+<div align="center">
+
+  ![DoWhile](frontend/src/assets/logo.svg)
+
+</div>
+
+
+<h2>Índice</h2>
+
+- [⚙️ Tecnologias](#️-tecnologias)
+- [📖 Projeto](#-projeto)
+- [🖌️ Layout](#️-layout)
+- [🚀 Features de minha autoria](#-features-de-minha-autoria)
+
+---
+
+<div  align="center">
+
+  ![DoWhile 2021](https://user-images.githubusercontent.com/67246528/138088767-84309a13-0ca9-44d6-8df3-9bfb25731cb5.png)
+
+</div>
+
+> Design da aplicação
+
+### ⚙️ Tecnologias
+
+Esse projeto foi feito com as seguintes tecnologias.
+
+- ReactTS
+- TypeScript
+- SCSS
+- NodeJS
+- Prisma
+- Socket.io
+
+### 📖 Projeto
+
+Aplicação fullstack feita no NLW para o evento DoWhile 2021. A aplicação contém comunicação real time com o servidor node e integrações websockets.
+
+### 🖌️ Layout
+
+Disponibilizado pela Rocketseat e utilizado como base para o projeto:
+
+- [Layout](https://www.figma.com/community/file/1031699316177416916)
+
+### 🚀 Features de minha autoria
+
+- Adicionado um tratamento visual para os erros e confirmações de envios usando o react-hot-toast.
+- Deploy da aplicação no Heroku(back-end) na Vercel(front-end).
+- Refatoração do contexto.
+- Novo design visual.
+- Preloader para a página;
+- Efeitos no carregamento dinâmico das imagens
+
+---
+<p align="center">
+  Made with 💘 by Erebor 👋🏾
+</p>
---
+++ frontend/src/components/MessageList/index.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+import { api } from '../../services/api'
+
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+import { Preloader } from '../Preloader'
+
+type Message = {
+  id: string,
+  text: string,
+  user: {
+    name: string,
+    avatar_url: string,
+  },
+  created_at?: string;
+}
+
+let messagesQueue: Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', newMessage => {
+  messagesQueue.push(newMessage)
+})
+
+export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    setInterval(() => {
+      if(messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean))
+
+        messagesQueue.shift()
+      }
+    }, 3000)
+  }, [])
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data)
+    })
+  }, [])
+
+  async function isVisible() {
+    const [loading, setloading] = useState(false)
+
+      setInterval(() => {
+      if (messages.length > 0) {
+        return loading == true;
+        
+      }
+        return setloading(false);
+      }, 3000)
+  }
+
+  return (
+    <>
+      {! isVisible() ? <Preloader /> : <div className={styles.messageListWrapper}>
+    <img src={logoImg} alt="DoWhile 2021" />
+
+    <ul className={styles.messageList}>
+      {messages.map(message => {
+        return (
+        <li key={message.created_at} className={styles.message}>
+          <p className={styles.messageContent}>{message.text}</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src={message.user.avatar_url} alt={message.user.name} />
+            </div>
+            <span>{message.user.name}</span>
+          </div>
+        </li>
+        )
+      })}
+    </ul>
+  </div>}
+    </>
+  )
+}
